c9b5a2c51e517ca3d059b1ff7e5e83dba1c7239a
Manish Solanki <152526919+manishkumar0015@users.noreply.github.com>
2024-10-04 08:32:58 +0530

    Add files via upload

---
+++ fancybox/js/script.js
@@ -0,0 +1,10 @@
+$('[data-fancybox]').fancybox({
+    buttons: [
+        'close'
+    ],
+    wheel: false,
+    transitionEffect: "",
+    loop: false,
+    toolbar: false,
+    clickContent: false
+});
